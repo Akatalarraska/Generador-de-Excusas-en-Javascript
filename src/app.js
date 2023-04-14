38ea4c30e94d8a62c70ea37645d457b4a696ff84
Akatalarraska <125604224+Akatalarraska@users.noreply.github.com>
2023-04-14 19:11:47 +0000

    generador de excusas funcionando - pndt optimizar código

--- src/app.js
+++ src/app.js
@@ -1,12 +1,34 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
+window.onload = () => {
   //write your code here
-  console.log("Hello Rigo from the console!");
-  console.log("Hola hola try 1")
+  document.querySelector("#excuse").innerHTML = autoexcuse();
 };
+
+const autoexcuse = () => {
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  let whoindex = Math.floor(Math.random() * who.length);
+  let actionindex = Math.floor(Math.random() * action.length);
+  let whatindex = Math.floor(Math.random() * what.length);
+  let whenindex = Math.floor(Math.random() * when.length);
+
+  return (
+    who[whoindex] +
+    " " +
+    action[actionindex] +
+    " " +
+    what[whatindex] +
+    " " +
+    when[whenindex]
+  );
+};
+
+console.log(autoexcuse());
